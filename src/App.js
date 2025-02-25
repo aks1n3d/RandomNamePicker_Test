a2351b130f27f7a4b44e20da267d27c3a5c08414
Aks1n3d <denys.busiak@gmail.com>
2025-02-25 21:29:35 +0100

    increased speed of roulet

--- src/App.js
+++ src/App.js
@@ -155,7 +155,7 @@ function Drum({ people, lastWinner, onWinner, onBack }) {
     function spin(namesArray, containerWidth) {
         setOffset(0);
 
-        let velocity = 1500 + Math.random() * 500;
+        let velocity = 1550 + Math.random() * 525;
         let friction = 0.988 + Math.random() * 0.01;
         let currentOffset = Math.random() * (namesArray.length * 160);
         let lastTimestamp = 0;
